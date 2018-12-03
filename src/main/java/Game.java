a111f80ec5a83991f2073531ff577a6dd28c36ca
Wojciech T <hadwao2@gmail.com>
2018-12-03 18:05:02 +0100

    zad_dom22_v3

---
+++ src/main/java/Game.java
@@ -0,0 +1,65 @@
+public class Game {
+    private String title;
+    private String producer;
+    private String sort;
+    private String type;
+    private Integer price;
+    private Integer rating;
+
+    public Game(String title, String producer, String sort, String type, Integer price, Integer rating) {
+        this.title = title;
+        this.producer = producer;
+        this.sort = sort;
+        this.type = type;
+        this.price = price;
+        this.rating = rating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public String getSort() {
+        return sort;
+    }
+
+    public void setSort(String sort) {
+        this.sort = sort;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+}
