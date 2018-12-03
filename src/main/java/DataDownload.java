e4d89d1b0603acba34623a839206ea2c7e594e9b
WojciechTom <42958849+WojciechTom@users.noreply.github.com>
2018-12-03 18:11:50 +0100

    Update DataDownload.java

--- src/main/java/DataDownload.java
+++ src/main/java/DataDownload.java
@@ -1,12 +1,13 @@
 import java.io.IOException;
 import java.sql.*;
+import java.util.ArrayList;
 
 public class DataDownload {
 
     private String user;
     private String password;
-    Connection con;
-    Statement stat;
+    private Connection con;
+    private Statement stat;
 
 
      public DataDownload(String user, String password) throws ClassNotFoundException, SQLException {
@@ -16,26 +17,33 @@ public class DataDownload {
         stat = con.createStatement();
     }
 
-    public ResultSet ShowAll() throws SQLException, ClassNotFoundException, IOException {
+    public ArrayList<Game> showAll() throws SQLException, ClassNotFoundException, IOException {
         String query = "select * from games";
         ResultSet res = stat.executeQuery(query);
-        return res;
+        return resultSetToList(res);
     }
 
 
-    public ResultSet ShowGamesAboveLevel(int MinRating) throws SQLException, IOException {
+    public ArrayList<Game> showGamesAboveLevel(int MinRating) throws SQLException, IOException {
         String query = "select * from games where rating > " + MinRating + " ";
         ResultSet res = stat.executeQuery(query);
-        return res;
+        return resultSetToList(res);
     }
 
 
-    public ResultSet ShowComputerGames() throws SQLException, IOException {
+    public ArrayList<Game> showComputerGames() throws SQLException, IOException {
         String query = "select * from games where sort  = \"computer game\" ";
         ResultSet res = stat.executeQuery(query);
-        return res;
+        return resultSetToList(res);
     }
 
-
+    private ArrayList<Game> resultSetToList(ResultSet res) throws SQLException {
+        ArrayList<Game> listaGier = new ArrayList<Game>();
+        while (res.next()) {
+            Game gra = new Game(res.getString("title"), res.getString("producer"), res.getString("sort"), res.getString("type"), res.getInt("price"), res.getInt("rating"));
+            listaGier.add(gra);
+        }
+        return listaGier ;
+     }
 
 }
